ed881ce7c777f2f6b7d798e509bb69c603914f1b
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-06 02:15:15 +0000

    Added new options to form

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2022 Maria Goemans All rights reserved
+//
+// Created by: Maria G
+// Created on: May 3, 2022
+// This file contains the JS functions for index.html
+
+"use strict"

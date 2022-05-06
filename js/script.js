49fe87817c7a4159010f1470f17c4e52363ba912
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-06 19:23:10 +0000

    Added/fixed spacing in JS file, improving readability

--- js/script.js
+++ js/script.js
@@ -9,18 +9,23 @@
 function orderSent() {
   //setting a constant for tax
   const HST = 0.13;
+  
   //initializing variables
   let baseSandCost = 0;
+  
   //declaring variables for base sandwich and sides
   let meatType = document.getElementById("meat-type").value;
   let sandSize = document.getElementById("sand-size").value;
   let numDrinks = parseInt(document.getElementById("drinks").value);
   let numPotatoes = parseInt(document.getElementById("potatoes").value);
+  
   //determining the number of checkboxes checked (number of toppings selected) - method found at https://www.techiedelight.com/count-number-check-boxes-javascript/
   let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
   let numToppings = checkboxes.length;
+  
   //determining the cost of the toppings using a variable
   let costToppings = numToppings * 0.25;
+  
   //determining the cost of the base sandwich using compound if/then statements
   //if the meat type is chicken and the size is small
   if ((meatType == "chicken") && (sandSize == "small")) {
@@ -46,12 +51,16 @@ function orderSent() {
   else {
     baseSandCost = 8.50;
   }
+  
   //determining the cost of the sides
   let costDrinks = numDrinks * 3.00;
   let costPotatoes = numPotatoes * 6.00;
-  //calculating the subtotal
+  
+  //calculating the subtotal, tax and total
   let subtotal = baseSandCost + costToppings + costDrinks + costPotatoes;
   let tax = subtotal * HST;
   let total = subtotal + tax;
+
+  //displaying the totals to the screen
   document.getElementById("results").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
 }
